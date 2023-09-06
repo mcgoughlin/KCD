a95e3b422c8d3cd1fb32f9fbd81acb0db16be8a8
William McGough <billy.mcgough1@hotmail.com>
2023-09-06 10:30:01 +0100

    Python Package Files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='KCD_early',
+    packages=['KCD_early'],
+    description='Python Package for the early detection of renal cancer',
+    version='0.1',
+    url='https://github.com/mcgoughlin/KCD',
+    author='bmcgough',
+    author_email='billy.mcgough1@hotmail.com',
+    keywords=['pip','pytorch','cancer']
+    )
