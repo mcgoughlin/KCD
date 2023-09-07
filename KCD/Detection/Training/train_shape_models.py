54837b2cfbdfa2b4be56d4e8ac349ef358ffd2db
William McGough <billy.mcgough1@hotmail.com>
2023-09-07 15:03:11 +0100

    Transfer training functions to utils file

--- KCD/Detection/Training/train_shape_models.py
+++ KCD/Detection/Training/train_shape_models.py
@@ -5,130 +5,10 @@ import warnings
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
 from sklearn.model_selection import StratifiedKFold as kfold_strat
-import dgl
-import KCD.Detection.Dataloaders.object_dataloader as dl_shape
 import KCD.Detection.Evaluation.eval_scripts as eval_
 from KCD.Detection.ModelGenerator import model_generator
-
-
-def initialize_device():
-    if torch.cuda.is_available():
-        return 'cuda'
-    else:
-        return 'cpu'
-
-
-def shape_collate(samples, dev=initialize_device()):
-    features, graphs, labels = map(list, zip(*samples))
-    batched_graph = dgl.batch(graphs)
-    return torch.stack(features).to(dev), batched_graph.to(dev), torch.stack(labels).squeeze(dim=0).to(dev)
-
-
-def create_directory(path):
-    if not os.path.exists(path):
-        os.mkdir(path)
-
-
-def init_params():
-    params = {
-        'tw_size': 'large',
-        'sv_size': 'small',
-        'gnn_layers': 5,
-        'gnn_hiddendim': 25,
-        'gnn_neighbours': 2,
-        'object_batchsize': 8,
-        'graph_thresh': 500,
-        'mlp_thresh': 20000,
-        'combined_threshold':500,
-        'mlp_lr': 0.01,
-        'gnn_lr': 0.001,
-        's1_objepochs': 100,
-        'shape_unfreeze_epochs': 2,
-        'shape_freeze_epochs': 25,
-        'shape_freeze_lr':1e-3,
-        'shape_unfreeze_lr': 1e-3,
-        'combined_threshold':500,
-        'ensemble_n1':128,
-        'ensemble_n2':32
-    }
-    return params
-
-
-def check_params(params):
-    inputset = set(params.keys())
-    checkset = set(init_params.keys())
-    assert(checkset.issubset(inputset))
-
-
-def init_paths(home, dataname):
-    save_dir = os.path.join(home, dataname)
-    create_directory(home),create_directory(save_dir)
-    return save_dir
-
-
-def get_data(obj_path, dataname, graph_thresh, mlp_thresh,ensemble=False):
-    shapedataset = dl_shape.ObjectData_labelled(obj_path, data_name=dataname, graph_thresh=graph_thresh, mlp_thresh=mlp_thresh,ensemble=ensemble)
-    test_shapedataset = dl_shape.ObjectData_labelled(obj_path, data_name=dataname, graph_thresh=0, mlp_thresh=0,ensemble=ensemble)
-    return shapedataset, test_shapedataset
-
-
-def get_cases(shapedataset):
-    cases = np.unique(shapedataset.cases.values)
-    is_ncct = np.array([0 if case.startswith('case') else 1 for case in cases])
-    return cases, is_ncct
-
-
-def train_model(dl, dev, epochs, loss_fnc, opt, model):
-    model.train()
-    for i in range(epochs):
-        print("\nEpoch {}".format(i))
-        for features,graph,label in dl:
-            pred = model(features.to(dev),graph.to(dev))
-            if label.numel() != 1:label = label.squeeze()
-            loss = loss_fnc(pred, label.to(dev))
-            loss.backward()
-            opt.step()
-            opt.zero_grad()
-            
-    return model
-
-
-def train_shape_models(dl, dev, epochs, loss_fnc, MLPopt, GNNopt, MLP, GNN):
-    MLP.train(), GNN.train()
-    for i in range(epochs):
-        print("\nEpoch {}".format(i))
-        for features, graph, lb in dl:
-            feat_lb, graph_lb = lb.T
-            MLPpred = MLP(features.to(dev))
-            GNNpred = GNN(graph.to(dev))
-            for pred, opt, label in zip([MLPpred, GNNpred], [MLPopt, GNNopt], [feat_lb, graph_lb]):
-                if label.numel() == 1: label = label.unsqueeze(0)
-                loss = loss_fnc(pred, label.to(dev))
-                loss.backward()
-                opt.step()
-                opt.zero_grad()
-                
-    MLP.eval(),GNN.eval()
-                
-    return MLP,GNN
-
-
-def generate_dataloaders(dataset,test_dataset,train_index,cases,batch_size,collate_fn):
-    dataset.apply_foldsplit(train_cases = cases[train_index])
-    test_dataset.apply_foldsplit(train_cases = cases[train_index])
-    dl = DataLoader(dataset,batch_size=batch_size,shuffle=True,collate_fn=collate_fn)
-    test_dl = DataLoader(test_dataset,batch_size=batch_size,shuffle=True,collate_fn=collate_fn)
-    return dl, test_dl
-
-
-def plot_roc(name,model_name,ROC):
-    sens, spec = ROC[:, 0] * 100, ROC[:, 1] * 100
-    AUC = np.trapz(sens, spec) / 1e4
-    plt.plot(100 - spec, sens, linewidth=2, label=name + ' AUC {:.3f}'.format(AUC))
-    plt.ylabel('Sensivity / %')
-    plt.xlabel('100 - Specificity / %')
+from KCD.Detection.Training import train_utils as tu
     
 
 def train_cv_individual_models(home = '/Users/mcgoug01/Downloads/Data/',dataname='merged_training_set',
@@ -137,14 +17,14 @@ def train_cv_individual_models(home = '/Users/mcgoug01/Downloads/Data/',dataname
     warnings.filterwarnings("ignore") #makes dgl stop complaining!
 
     # Initialization
-    dev = initialize_device()
+    dev = tu.initialize_device()
     
-    if params==None:params = init_params()
-    else:check_params(params)
+    if params==None:params = tu.init_shape_params()
+    else:tu.check_params(params)
     
-    save_dir = init_paths(os.path.join(home,'training_info'), dataname)
-    shapedataset, test_shapedataset = get_data(home, dataname, params['graph_thresh'], params['mlp_thresh'],ensemble=True)
-    cases, is_ncct = get_cases(shapedataset)
+    save_dir = tu.init_paths(os.path.join(home,'training_info'), dataname)
+    shapedataset, test_shapedataset = tu.get_shape_data(home, dataname, params['graph_thresh'], params['mlp_thresh'],ensemble=False)
+    cases, is_ncct = tu.get_cases(shapedataset)
 
     # More Initialization
     loss_fnc = nn.CrossEntropyLoss().to(dev)
@@ -180,8 +60,8 @@ def train_cv_individual_models(home = '/Users/mcgoug01/Downloads/Data/',dataname
             GNNopt = torch.optim.Adam(GNN.parameters(),lr=params['gnn_lr'])
             MLPopt = torch.optim.Adam(MLP.parameters(),lr=params['mlp_lr'])
     
-            dl,test_dl = generate_dataloaders(shapedataset,test_shapedataset,train_index,cases,params['object_batchsize'],shape_collate)
-            MLP,GNN = train_shape_models(dl,dev,params['s1_objepochs'],loss_fnc,MLPopt,GNNopt,MLP,GNN)
+            dl,test_dl = tu.generate_dataloaders(shapedataset,test_shapedataset,train_index,cases,params['object_batchsize'],tu.shape_collate)
+            MLP,GNN = tu.train_shape_models(dl,dev,params['s1_objepochs'],loss_fnc,MLPopt,GNNopt,MLP,GNN)
             
             MLP_name = '{}_{}_{}_{}'.format(params['s1_objepochs'],params['mlp_thresh'],params['mlp_lr'],params['object_batchsize'])
             GNN_name = '{}_{}_{}_{}_{}_{}_{}'.format(params['s1_objepochs'],params['graph_thresh'],params['gnn_lr'],params['gnn_layers'],params['gnn_hiddendim'],params['gnn_neighbours'],params['object_batchsize'])
@@ -211,15 +91,13 @@ def train_cv_individual_models(home = '/Users/mcgoug01/Downloads/Data/',dataname
         np.save(os.path.join(split_path, 'GNN_ROC_'+GNN_name), GNN_ROC)
 
         fig = plt.figure(figsize=(8, 6))
-        plot_roc('MLP',MLP_name,MLP_ROC)
-        plot_roc('GNN',GNN_name,GNN_ROC)
+        tu.plot_roc('MLP',MLP_name,MLP_ROC)
+        tu.plot_roc('GNN',GNN_name,GNN_ROC)
         plt.legend()
         plt.savefig(os.path.join(split_path, 'ShapeModels_ROC_'+MLP_name + '+' + GNN_name + '.png'))
         plt.show()
         plt.close()
         
-        return MLP,GNN
-
 
 def train_cv_shape_ensemble(home = '/Users/mcgoug01/Downloads/Data/',dataname='merged_training_set',
                          splits:list=[0],params:dict=None,folds=5):
@@ -227,12 +105,12 @@ def train_cv_shape_ensemble(home = '/Users/mcgoug01/Downloads/Data/',dataname='m
     warnings.filterwarnings("ignore") #makes dgl stop complaining!
 
     # Initialization
-    dev = initialize_device()
-    if params==None:params = init_params()
-    else:check_params(params)
-    save_dir = init_paths(os.path.join(home,'training_info'), dataname)
-    shapedataset, test_shapedataset = get_data(home, dataname, params['combined_threshold'], params['combined_threshold'],ensemble=True)
-    cases, is_ncct = get_cases(shapedataset)
+    dev = tu.initialize_device()
+    if params==None:params = tu.init_shape_params()
+    else:tu.check_params(params)
+    save_dir = tu.init_paths(os.path.join(home,'training_info'), dataname)
+    shapedataset, test_shapedataset = tu.get_shape_data(home, dataname, params['combined_threshold'], params['combined_threshold'],ensemble=True)
+    cases, is_ncct = tu.get_cases(shapedataset)
 
     # More Initialization
     loss_fnc = nn.CrossEntropyLoss().to(dev)
@@ -258,7 +136,7 @@ def train_cv_shape_ensemble(home = '/Users/mcgoug01/Downloads/Data/',dataname='m
             ensemble_path = os.path.join(fold_path,'shape_ensemble')
             MLP_path = os.path.join(fold_path,'MLP')
             GNN_path = os.path.join(fold_path,'GNN')
-            dl,test_dl = generate_dataloaders(shapedataset,test_shapedataset,train_index,cases,params['object_batchsize'],shape_collate)
+            dl,test_dl = tu.generate_dataloaders(shapedataset,test_shapedataset,train_index,cases,params['object_batchsize'],tu.shape_collate)
 
             if not os.path.exists(ensemble_path): os.mkdir(ensemble_path)
                 
@@ -283,11 +161,11 @@ def train_cv_shape_ensemble(home = '/Users/mcgoug01/Downloads/Data/',dataname='m
                                               list(ShapeEnsemble.MLP.skip2.parameters())+
                                               list(ShapeEnsemble.GNN.classify2.parameters()),lr=params['shape_freeze_lr'])
     
-                ShapeEnsemble = train_model(dl,dev,params['shape_freeze_epochs'],loss_fnc,SEopt,ShapeEnsemble)
+                ShapeEnsemble = tu.train_model(dl,dev,params['shape_freeze_epochs'],loss_fnc,SEopt,ShapeEnsemble)
             if params['shape_unfreeze_epochs']>0:
                 print('Unfrozen model training')
                 SEopt = torch.optim.Adam(ShapeEnsemble.parameters(),lr=params['shape_unfreeze_lr'])
-                ShapeEnsemble = train_model(dl,dev,params['shape_unfreeze_epochs'],loss_fnc,SEopt,ShapeEnsemble)
+                ShapeEnsemble = tu.train_model(dl,dev,params['shape_unfreeze_epochs'],loss_fnc,SEopt,ShapeEnsemble)
 
             ShapeEnsemble.eval()
 
@@ -305,14 +183,13 @@ def train_cv_shape_ensemble(home = '/Users/mcgoug01/Downloads/Data/',dataname='m
         np.save(os.path.join(split_path, 'Ensemble_ROC_'+name), ensemble_ROC)
 
         fig = plt.figure(figsize=(8, 6))
-        plot_roc('Shape Ensemble',name,ensemble_ROC)
+        tu.plot_roc('Shape Ensemble',name,ensemble_ROC)
         plt.legend()
         plt.savefig(os.path.join(split_path,'Ensemble_ROC_' +name+ '.png'))
         plt.show()
         plt.close()
         
-        return ShapeEnsemble
-
 if __name__ == '__main__':
-    # MLP,GNN = train_individual_models()
-    train_cv_shape_ensemble()
+    dataset = 'coreg_ncct'
+    train_cv_individual_models(dataname=dataset)
+    train_cv_shape_ensemble(dataname=dataset)
---
+++ KCD/Detection/Training/train_utils.py
@@ -0,0 +1,121 @@
+import KCD.Detection.Dataloaders.object_dataloader as dl_shape
+from torch.utils.data import DataLoader
+import numpy as np
+import os
+import torch
+import dgl
+
+
+def initialize_device():
+    if torch.cuda.is_available():
+        return 'cuda'
+    else:
+        return 'cpu'
+
+
+def create_directory(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+        
+        
+def init_paths(home, dataname):
+    save_dir = os.path.join(home, dataname)
+    create_directory(home),create_directory(save_dir)
+    return save_dir
+        
+
+def shape_collate(samples, dev=initialize_device()):
+    features, graphs, labels = map(list, zip(*samples))
+    batched_graph = dgl.batch(graphs)
+    return torch.stack(features).to(dev), batched_graph.to(dev), torch.stack(labels).squeeze(dim=0).to(dev)
+
+
+def init_shape_params():
+    params = {
+        'tw_size': 'large',
+        'sv_size': 'small',
+        'gnn_layers': 5,
+        'gnn_hiddendim': 25,
+        'gnn_neighbours': 2,
+        'object_batchsize': 8,
+        'graph_thresh': 500,
+        'mlp_thresh': 20000,
+        'combined_threshold':500,
+        'mlp_lr': 0.01,
+        'gnn_lr': 0.001,
+        's1_objepochs': 100,
+        'shape_unfreeze_epochs': 2,
+        'shape_freeze_epochs': 25,
+        'shape_freeze_lr':1e-3,
+        'shape_unfreeze_lr': 1e-3,
+        'combined_threshold':500,
+        'ensemble_n1':128,
+        'ensemble_n2':32
+    }
+    return params
+
+
+def check_params(params):
+    inputset = set(params.keys())
+    checkset = set(init_shape_params().keys())
+    assert(checkset.issubset(inputset))
+    
+
+def get_cases(dataset):
+    cases = np.unique(dataset.cases.values)
+    is_ncct = np.array([0 if case.startswith('case') else 1 for case in cases])
+    return cases, is_ncct
+
+def get_shape_data(obj_path, dataname, graph_thresh, mlp_thresh,ensemble=False):
+    shapedataset = dl_shape.ObjectData_labelled(obj_path, data_name=dataname, graph_thresh=graph_thresh, mlp_thresh=mlp_thresh,ensemble=ensemble)
+    test_shapedataset = dl_shape.ObjectData_labelled(obj_path, data_name=dataname, graph_thresh=0, mlp_thresh=0,ensemble=ensemble)
+    return shapedataset, test_shapedataset
+
+
+def train_model(dl, dev, epochs, loss_fnc, opt, model):
+    model.train()
+    for i in range(epochs):
+        print("\nEpoch {}".format(i))
+        for features,graph,label in dl:
+            pred = model(features.to(dev),graph.to(dev))
+            if label.numel() != 1:label = label.squeeze()
+            loss = loss_fnc(pred, label.to(dev))
+            loss.backward()
+            opt.step()
+            opt.zero_grad()
+            
+    return model
+
+
+def train_shape_models(dl, dev, epochs, loss_fnc, MLPopt, GNNopt, MLP, GNN):
+    MLP.train(), GNN.train()
+    for i in range(epochs):
+        print("\nEpoch {}".format(i))
+        for features, graph, lb in dl:
+            feat_lb, graph_lb = lb.T
+            MLPpred = MLP(features.to(dev))
+            GNNpred = GNN(graph.to(dev))
+            for pred, opt, label in zip([MLPpred, GNNpred], [MLPopt, GNNopt], [feat_lb, graph_lb]):
+                if label.numel() == 1: label = label.unsqueeze(0)
+                loss = loss_fnc(pred, label.to(dev))
+                loss.backward()
+                opt.step()
+                opt.zero_grad()
+                
+    MLP.eval(),GNN.eval()
+                
+    return MLP,GNN
+
+def generate_dataloaders(dataset,test_dataset,train_index,cases,batch_size,collate_fn):
+    dataset.apply_foldsplit(train_cases = cases[train_index])
+    test_dataset.apply_foldsplit(train_cases = cases[train_index])
+    dl = DataLoader(dataset,batch_size=batch_size,shuffle=True,collate_fn=collate_fn)
+    test_dl = DataLoader(test_dataset,batch_size=batch_size,shuffle=True,collate_fn=collate_fn)
+    return dl, test_dl
+
+def plot_roc(name,model_name,ROC):
+    sens, spec = ROC[:, 0] * 100, ROC[:, 1] * 100
+    AUC = np.trapz(sens, spec) / 1e4
+    plt.plot(100 - spec, sens, linewidth=2, label=name + ' AUC {:.3f}'.format(AUC))
+    plt.ylabel('Sensivity / %')
+    plt.xlabel('100 - Specificity / %')
