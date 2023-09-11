75faceb0e2d25fed4ac612d4fe61de30d2361c97
mcgoughlin <billy.mcgough1@hotmail.com>
2023-09-11 16:09:54 +0100

    update setup.py file

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     #           'scikit-image==0.19.3','scikit-learn==1.2.2','scipy==1.10.1',
     #           'simpleitk==2.2.1','torchvision==0.15.2'],
     install_requires=['torch','dgl','matplotlib','nibabel','bpy',
-              'numpy','opencv-python','pandas','pydicom','stl'
+              'numpy','opencv-python','pandas','pydicom','stl',
               'scikit-image','scikit-learn','scipy','numpy-stl',
               'simpleitk','torchvision','pymeshfix','open3d'],
-    python_requires='>=3.6, <=3.8',
+    python_requires='>=3.6, <=3.10.7',
     description='Python Package for the early detection of renal cancer',
     version='0.1',
     url='https://github.com/mcgoughlin/KCD',
