cc102ff3436b123e8babc0a2cf0d65530ef09603
William McGough <billy.mcgough1@hotmail.com>
2023-09-13 14:47:58 +0100

    correct inference_dataset.py for segmentation inference test

--- KCD/Segmentation/HPC_Scripts/inference_dataset.py
+++ KCD/Segmentation/HPC_Scripts/inference_dataset.py
@@ -1,15 +1,16 @@
+import os
+os.environ['OV_DATA_BASE'] = 'basker/projects/p/phwq4930-renal-canc/data/seg_data'
+
 from KCD.Segmentation.Inference.EnsembleSeg import Ensemble_Seg
 import numpy as np
 
-# seg_fp = "/home/wcm23/rds/hpc-work/CascadeSegmentationFiles/cascade_stage1/"
-home = 'basker/projects/p/phwq4930-renal-canc/data/seg_data'
 seg_fp = '/bask/projects/p/phwq4930-renal-canc/data/seg_data/trained_models/coreg_ncct/4.0mm_allbinary_noR74K118/6,3x3x3,32_pretrained_noerrors'
 data_names = ['coreg_ncct']
 do_prep = True
 do_infer = True
 
 for data_name in data_names:
-    test = Ensemble_Seg(home, data_name,
+    test = Ensemble_Seg(os.environ['OV_DATA_BASE'], data_name,
                         seg_fp=seg_fp, seg_dev='cuda',
                         spacing=np.array([4] * 3),
                         do_prep=do_prep, do_infer=do_infer)
