7af067eaf445c5ef3c9fd187976ec025eb831775
mcgoughlin <billy.mcgough1@hotmail.com>
2023-09-18 11:48:50 +0100

    Add scripts for pretraining on kits23sncct and other data

--- KCD/Detection/Training/train_slice_models_pretrain.py
+++ KCD/Detection/Training/train_slice_models_pretrain.py
@@ -92,4 +92,4 @@ def train_cv_slice_model(home = '/Users/mcgoug01/Downloads/Data/',dataname='core
 if __name__ == '__main__':
     dataset = 'kits23sncct'
     home = '/bask/projects/p/phwq4930-renal-canc/KCD_data/Data'
-    train_cv_slice_model(home=home,dataname=dataset,is_3D=False,splits=[0])
+    train_cv_slice_model(home=home,dataname=dataset,is_3D=True,splits=[0,1])
